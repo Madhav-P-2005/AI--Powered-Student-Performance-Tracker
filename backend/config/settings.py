27e2cdb0acd387af501249ad6710965c3acdeb79
Madhav P <163096888+Madhav-P-2005@users.noreply.github.com>
2026-03-22 10:19:31 +0530

    Change Brevo SMTP port to 2525 for Render

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -203,7 +203,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 2525
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('BREVO_SMTP_LOGIN', '')
 EMAIL_HOST_PASSWORD = os.environ.get('BREVO_SMTP_KEY', '')
