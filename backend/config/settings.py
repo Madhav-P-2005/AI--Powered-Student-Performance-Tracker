eb2a35c4261a8f48b95693e7f7a968baca63eacf
Madhav P <163096888+Madhav-P-2005@users.noreply.github.com>
2026-05-19 17:30:18 +0530

    Backend Updated

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -206,6 +206,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 2525
 EMAIL_USE_TLS = True
+EMAIL_TIMEOUT = 5  # Prevent infinite hang if Render blocks SMTP ports
 EMAIL_HOST_USER = os.environ.get('BREVO_SMTP_LOGIN', '')
 EMAIL_HOST_PASSWORD = os.environ.get('BREVO_SMTP_KEY', '')
 DEFAULT_FROM_EMAIL = os.environ.get('BREVO_SENDER_EMAIL', 'noreply@aistudenttracker.com')
